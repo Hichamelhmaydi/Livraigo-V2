fb79261c3f6e28fef815ce0089f2d50376461dec
Hichamelhmaydi <ct1594065@gmail.com>
2025-11-17 11:52:24 +0100

    Remove SpringBootServletInitializer and simplify application configuration

--- src/main/java/com/livraigo/LivraigoApplication.java
+++ src/main/java/com/livraigo/LivraigoApplication.java
@@ -2,10 +2,9 @@ package com.livraigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = "com.livraigo")
-public class LivraigoApplication extends SpringBootServletInitializer {
+@SpringBootApplication
+public class LivraigoApplication {
     public static void main(String[] args) {
         SpringApplication.run(LivraigoApplication.class, args);
     }
